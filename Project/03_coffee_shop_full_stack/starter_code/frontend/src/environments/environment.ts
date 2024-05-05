00b60797ca1375f940d3ce8693045d31954c21a9
VaglesBagels <104954408+VaglesBagels@users.noreply.github.com>
2024-05-06 08:02:34 +1000

    Frontend edit

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
-/* @TODO replace with your variables
+/* @DONE replace with your variables
  * ensure all variables on this page match your project
  */
+import {DOMAIN, API_AUDIENCE, AUTH_CLIENT_ID} from '../../secrets.js';
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: DOMAIN, // the auth0 domain prefix
+    audience: API_AUDIENCE, // the audience set for the auth0 app
+    clientId: AUTH_CLIENT_ID, // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
